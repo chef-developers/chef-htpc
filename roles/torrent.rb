4659b3d4cb3c124ba9a654bcc8f9ecda67c7530f
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-07-01 19:17:25 +0200

    transmission setup

--- roles/torrent.rb
+++ roles/torrent.rb
@@ -4,6 +4,10 @@ run_list 'transmission'
 
 override_attributes(
   'transmission' => {
-    'rpc_password' => 'transmission123'
+    'rpc_password' => 'transmission123',
+    'rpc-whitelist-enabled' => 'false',
+    'download_dir' => '/srv/media/Download',
+    'incomplete_dir' => '/srv/media/incomplete',
+    'incomplete_dir_enabled' => 'true'
   }
 )
