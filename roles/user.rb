7f3c106868a921e0a6b6a4d5c9173c6294c10937
Lukasz Kaniowski <lukasz.kaniowski@gmail.com>
2012-07-01 15:21:28 +0200

    sudo

--- roles/user.rb
+++ roles/user.rb
@@ -1,7 +1,14 @@
 name "user"
 
-run_list "user::data_bag"
+run_list "user::data_bag", "sudo"
 
 override_attributes(
-  'users' => 'ukache' # you need to specify which users from databag will be set for node
+  'users' => 'ukache', # you need to specify which users from databag will be set for node
+    "authorization" => {
+      "sudo" => {
+        "groups" => ["admin", "wheel", "sysadmin"],
+        "users" => ["ukache"],
+        "passwordless" => true
+      }
+    }
 )
